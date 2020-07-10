df00265dd8196f05d48e4e941c6dba1fd27dd196
thesoftsys <thesoftsys.com@gmail.com>
2020-07-10 14:27:30 +0530

    add dbfile

--- includes/online_exam.sql
+++ includes/online_exam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 12, 2020 at 07:15 PM
+-- Generation Time: Jul 10, 2020 at 10:56 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -39,7 +39,8 @@ CREATE TABLE `add_course` (
 
 INSERT INTO `add_course` (`id`, `cname`) VALUES
 (16, 'Php'),
-(17, 'Java');
+(17, 'Java'),
+(18, 'Python');
 
 -- --------------------------------------------------------
 
@@ -63,7 +64,11 @@ CREATE TABLE `add_new_exam` (
 --
 
 INSERT INTO `add_new_exam` (`id`, `course_id`, `ename`, `nquestion`, `exam_time`, `pmax`, `status`, `equestionm`) VALUES
-(37, '17', 'Level 1', '25', '30', '2', '1', '1');
+(41, '16', 'Level 1', '10', '30', '65', '0', '2'),
+(42, '17', 'Level 2', '10', '30', '65', '0', '3'),
+(44, '16', 'Level 4', '10', '30', '1', '0', '1'),
+(45, '18', 'Level 10', '0', '30', '10', '0', '10'),
+(46, '16', 'Level 50', '10', '30', '1', '0', '12');
 
 -- --------------------------------------------------------
 
@@ -88,10 +93,24 @@ CREATE TABLE `add_questions` (
 --
 
 INSERT INTO `add_questions` (`id`, `question_no`, `exam_id`, `question`, `option_one`, `option_two`, `option_three`, `option_four`, `right_option`) VALUES
-(66, '1', '37', '1st ', 'a', 'b', 'c', 'd', 'a'),
-(67, '2', '37', '2nd', 'a', 'praj', 'japati', 'adjhj', 'abhi'),
-(68, '3', '37', '3rd', 'nahi', 'hai', 'koi ', 'bhi', 'bhi'),
-(69, '4', '37', '4th', 'tech', 'abhishek', 'programmer', 'abhi', 'programmer');
+(151, '2', '42', 'Where Is Java Used?', 'For Website Development', 'For App Development', 'For Software Development', 'For All', 'For All'),
+(152, '3', '42', 'What is the size of char variable?', '8 bit', '16 bit', '32 bit', '64 bit', '16 bit'),
+(153, '4', '42', 'What is the default value of String variable?', 'hjhj', 'jkj', 'kjkj', 'kjkj', 'kjkjk'),
+(163, '5', '42', 'askjdh', 'jhj', 'jhj', 'jhj', 'jhj', 'hjhj'),
+(164, '1', '41', 'hsh', 'gh', 'hgh', 'ghg', 'hgh', 'ghg'),
+(167, '3', '46', 'hgh', 'ghg', 'hgh', 'ghg', 'hgh', 'hghg'),
+(168, '4', '46', 'jhjh', 'jhj', 'hjh', 'jh', 'jh', 'jjhj'),
+(169, '5', '46', 'ghg', 'hghg', 'hghg', 'hghg', 'hghg', 'hgh'),
+(170, '6', '46', 'ghgh', 'ghgh', 'hgh', 'gh', 'ghg', 'hg'),
+(171, '7', '46', 'hghg', 'hghg', 'hghg', 'hghg', 'hghg', 'ghg'),
+(172, '8', '46', 'hghgh', 'ghghg', 'hghg', 'hghg', 'hghg', 'hghg'),
+(182, '9', '46', 'dfd', 'dfd', 'fdf', 'df', 'df', 'dfd'),
+(185, '10', '46', 'sdsghhg', 'hghg', 'hgjhg', 'jgjhg', 'jhgjhg', 'ghjgh'),
+(187, '2', '41', 'fg', 'gfgf', 'gfgfg', 'fgf', 'gfgf', 'gfgf'),
+(188, '3', '41', 'ab', 'jj', 'jj', 'hjhj', 'jj', 'jj'),
+(189, '4', '41', 'hh', 'jhjh', 'jhjh', 'jhj', 'hj', 'hjh'),
+(190, '5', '41', 'hjhh', 'jh', 'jhj', 'hjh', 'jhj', 'hj'),
+(191, '6', '41', 'hg', 'hgh', 'gh', 'gh', 'gh', 'ghssssss');
 
 -- --------------------------------------------------------
 
@@ -124,12 +143,24 @@ CREATE TABLE `exam_result` (
   `id` int(5) NOT NULL,
   `user_id` varchar(100) NOT NULL,
   `exam_id` varchar(100) NOT NULL,
+  `course_name` varchar(100) NOT NULL,
+  `exam_name` varchar(100) NOT NULL,
   `total_question` varchar(100) NOT NULL,
   `correct_answer` varchar(100) NOT NULL,
   `wrong_answer` varchar(100) NOT NULL,
   `exam_time` varchar(100) NOT NULL,
-  `percentage` varchar(100) NOT NULL
+  `marks_percentage` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `exam_result`
+--
+
+INSERT INTO `exam_result` (`id`, `user_id`, `exam_id`, `course_name`, `exam_name`, `total_question`, `correct_answer`, `wrong_answer`, `exam_time`, `marks_percentage`) VALUES
+(86, 'dcst2', '41', 'Php', 'Level 1', '3', '3', '0', '20-05-18', '100'),
+(87, 'dcst2', '42', 'Java', 'Level 2', '4', '4', '0', '20-05-18', '100'),
+(88, 'dcst1', '42', 'Java', 'Level 2', '4', '1', '3', '20-05-18', '25'),
+(97, 'Dcst1', '41', 'Php', 'Level 1', '3', '2', '1', '20-05-27', '66.666666666667');
 
 -- --------------------------------------------------------
 
@@ -153,10 +184,8 @@ CREATE TABLE `exam_student` (
 --
 
 INSERT INTO `exam_student` (`id`, `fullname`, `email`, `password`, `mobile`, `user_id`, `otp`, `role`) VALUES
-(15, 'sanjeet', 'sanjeet34@gmail.com', '12345', '231654', 'DCST1', '3015', 2),
-(17, 'Utkarsh', 'ut123@gmail.com', 'utk123', '336625252', 'DCST2', '6822', 2),
-(18, 'abhi', 'abhi23@gmail.com', 'abhi123', '9935325512', 'DCST3', '5017', 2),
-(19, 'student', 'student@gmail.com', 'stu123', '6252665', 'DCST4', '7529', 2);
+(35, 'Abhishek kumar', 'abhishek@gmail.com', 'abhi', '9506847777', 'DCST1', '7599', 2),
+(39, 'sanjeet', 'sanjeet@gmail.com', 'san', '8896556979', 'DCST2', '8779', 2);
 
 -- --------------------------------------------------------
 
@@ -176,11 +205,8 @@ CREATE TABLE `student_login` (
 --
 
 INSERT INTO `student_login` (`id`, `user_id`, `password`, `role`) VALUES
-(3, 'DCST1', '12345', 2),
-(4, 'DCST2', 'utk123', 2),
-(5, 'DCST2', 'utk123', 2),
-(6, 'DCST3', 'abhi123', 2),
-(7, 'DCST4', 'stu123', 2);
+(13, 'DCST1', 'abhi', 2),
+(17, 'DCST2', 'san', 2);
 
 --
 -- Indexes for dumped tables
@@ -236,19 +262,19 @@ ALTER TABLE `student_login`
 -- AUTO_INCREMENT for table `add_course`
 --
 ALTER TABLE `add_course`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `add_new_exam`
 --
 ALTER TABLE `add_new_exam`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `add_questions`
 --
 ALTER TABLE `add_questions`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=192;
 
 --
 -- AUTO_INCREMENT for table `exam_admin`
@@ -260,19 +286,19 @@ ALTER TABLE `exam_admin`
 -- AUTO_INCREMENT for table `exam_result`
 --
 ALTER TABLE `exam_result`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 
 --
 -- AUTO_INCREMENT for table `exam_student`
 --
 ALTER TABLE `exam_student`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `student_login`
 --
 ALTER TABLE `student_login`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
